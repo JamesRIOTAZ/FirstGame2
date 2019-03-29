f2352398f4fa8d4324b734d34dd4d5ca5eaa4074
JamesRIOTAZ <jmcdonald.uk@gmail.com>
2019-03-29 21:45:05 +0000

    Initial commit

---
+++ app/src/main/java/riotaz/com/firstgame/AboutMe.kt
@@ -0,0 +1,12 @@
+package riotaz.com.firstgame
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AboutMe :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_me)
+    }
+
+}
